a85622721b34118f47fbe3621993e53e845c7fcb
nataliakushnir <natalia.l.kushnir@gmail.com>
2015-07-22 23:19:52 +0300

    main

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from main.models import Order, Dish, Category
 
 
@@ -23,24 +22,4 @@ class OrderAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Dish, DishAdmin)
-=======
-from .models import Items, Orders, Order
-# Register your models here.
-
-
-class ItemsAdmin(admin.ModelAdmin):
-    model = Items
-    list_display = ('item_name', 'item_description', 'item_composition', 'item_weight', 'item_price', 'item_status', 'item_date_add')
-
-class OrdersAdmin(admin.ModelAdmin):
-    model = Orders
-    list_display = ('order_date_published', 'order_date_target', 'order_status')
-
-class OrderAdmin(admin.ModelAdmin):
-    model = Order
-    list_display = ('Orders.order_items', )
-
-admin.site.register(Items, ItemsAdmin)
-admin.site.register(Orders, OrdersAdmin)
->>>>>>> da983a13e6069f377d4068c3be960c08e95239dd
 admin.site.register(Order, OrderAdmin)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -46,50 +45,3 @@ class Order(models.Model):
         return a[1:]
 
     my_url_field.short_description = 'Column description'
-=======
-from django.db import models
-from django.db.models.fields.related import ManyToManyRel
-import datetime
-from django.utils import timezone
-
-# Create your models here.
-
-class Dishes(models.Model):
-    class Meta():
-        db_table = 'Dishes'
-
-    def __str__(self):
-        return self.dish_description
-    dish_description = models.TextField(max_length=200)
-
-    def __unicode__(self):
-        return u"%s" % self.dish_description
-
-class Items(models.Model,):
-    def __str__(self):
-        return self.item_name
-
-    def __unicode__(self):
-        return u"%s" % self.item_composition
-
-    def __unicode__(self):
-        return u"%s" % self.item_name
-
-    item_name = models.TextField(max_length=200)
-    item_description = models.ForeignKey(Dishes)
-    item_composition = models.TextField(max_length=200, blank=True)
-    item_weight = models.CommaSeparatedIntegerField(max_length=20)
-    item_price = models.FloatField(max_length=20)
-    item_status = models.BooleanField(default=False)
-    item_date_add = models.DateTimeField(auto_now_add=True)
-
-class Orders(models.Model):
-    order_date_published = models.DateTimeField(auto_now_add=True)
-    order_date_target = models.DateField()
-    order_item = models.ManyToManyField(Items)
-    order_status = models.BooleanField(default=True)
-
-class Order(models.Model):
-    order_items = models.ManyToManyField(Orders, related_name='order_items')
-
->>>>>>> da983a13e6069f377d4068c3be960c08e95239dd
